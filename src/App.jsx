c85a61037cfe378c4f3e718a775d9da0dae55279
Vladislav <vladislavmeza@gmail.com>
2025-07-05 12:26:04 +0300

    using React.createElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import ReactLogo from './assets/react.svg?react'
+import React from 'react'; // Важно: добавляем явный импорт React
+import ReactLogo from './assets/react.svg?react';
 import "./App.css";
 
 export const App = () => {
-	// Декларативный
-	return (
-		<div className="App">
-			<header className="App-header">
-				<ReactLogo />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="App-year">{new Date().getFullYear()}</div>
-			</header>
-		</div>
-		// Императивный Вызов функции new Date().getFullYear()
-	);
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement(
+      'header',
+      { className: 'App-header' },
+      React.createElement(ReactLogo, null),
+      React.createElement(
+        'p',
+        null,
+        'Edit ',
+        React.createElement('code', null, 'src/App.js'),
+        ' and save to reload.'
+      ),
+      React.createElement(
+        'a',
+        {
+          className: 'App-link',
+          href: 'https://reactjs.org',
+          target: '_blank',
+          rel: 'noopener noreferrer'
+        },
+        'Learn React'
+      ),
+      React.createElement(
+        'div',
+        { className: 'App-year' },
+        new Date().getFullYear()
+      )
+    )
+  );
 };
